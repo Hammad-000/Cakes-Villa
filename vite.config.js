b3469132147e173095182e0e6be29ffc35034eb3
Hammad Ahmed <syedhammad283@gmail.com>
2025-12-20 00:45:14 +0500

    dep

--- vite.config.js
+++ vite.config.js
@@ -2,14 +2,13 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),  tailwindcss()],
-  //  build: {
-  //   outDir: 'dist', 
-  //   sourcemap: true 
-  // }
+  plugins: [
+    react(),
+    tailwindcss({
+      content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+    }),
+  ],
 
-
-  
-})
+}
+)
